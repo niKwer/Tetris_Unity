5094d7a928b9b43e3bf0e90035d7e46337c8490b
Nikolai Zinenko <zinenko.nikolay.n@gmail.com>
2021-08-17 15:55:02 +0300

    Added rotation and moving features.

--- Assets/Scripts/TetrisBoard.cs
+++ Assets/Scripts/TetrisBoard.cs
@@ -9,6 +9,17 @@ public class TetrisBoard : MonoBehaviour
     public Tetromino currentTetromino { get; private set; }
     public ShapeData[] shapes;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
+
     private void Start()
     {
         SpawnTetromino();
@@ -21,17 +32,45 @@ public class TetrisBoard : MonoBehaviour
     private void SpawnTetromino()
     {
         int random = Random.Range(0, this.shapes.Length);
-        ShapeData shape = this.shapes[random];
-        this.currentTetromino.Initialize(spawnPosition, shape, this);
+        ShapeData shapeData = this.shapes[random];
+        this.currentTetromino.Initialize(spawnPosition, shapeData, this);
         Set(this.currentTetromino);
     }
 
-    private void Set(Tetromino tetromino)
+    public void Set(Tetromino tetromino)
     {
         for (int i = 0; i < tetromino.cells.Length; i++)
         {
             Vector3Int tilePosition = tetromino.cells[i] + tetromino.position;
-            this.tilemap.SetTile(tilePosition, tetromino.shape.tile);
+            this.tilemap.SetTile(tilePosition, tetromino.shapeData.tile);
         }
+    }
+
+    public void Clear(Tetromino tetromino)
+    {
+        for (int i = 0; i < tetromino.cells.Length; i++)
+        {
+            Vector3Int tilePosition = tetromino.cells[i] + tetromino.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Tetromino tetromino, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+        for (int i = 0; i < tetromino.cells.Length; i++)
+        {
+            Vector3Int tilePosition = tetromino.cells[i] + position;
+
+            if(!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+            if(this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,25 +1,118 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
-    public ShapeData shape { get; private set; }
+    public ShapeData shapeData { get; private set; }
     public Vector3Int position { get; private set; }
     public Vector3Int[] cells { get; private set; }
     public TetrisBoard board { get; private set; }
-    public void Initialize(Vector3Int position, ShapeData shape, TetrisBoard board)
+    public int rotationIndex { get; private set; }
+    public void Initialize(Vector3Int position, ShapeData shapeData, TetrisBoard board)
     {
-        this.shape = shape;
+        this.shapeData = shapeData;
         this.board = board;
         this.position = position;
+        this.rotationIndex = 0;
 
         if (this.cells == null)
-            this.cells = new Vector3Int[shape.cells.Length];
+            this.cells = new Vector3Int[shapeData.cells.Length];
 
-        for (int i = 0; i < shape.cells.Length; i++)
+        for (int i = 0; i < shapeData.cells.Length; i++)
         {
-            this.cells[i] = (Vector3Int)shape.cells[i];
+            this.cells[i] = (Vector3Int)shapeData.cells[i];
         }
+    }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+        if(Input.GetButtonDown("Rotate"))
+        {
+            Rotate(1);
+        }
+        if (Input.GetButtonDown("MoveRight"))
+        {
+            Move(Vector2Int.right);
+        }
+        else if (Input.GetButtonDown("MoveLeft"))
+        {
+            Move(Vector2Int.left);
+        }
+
+        if (Input.GetButtonDown("MoveDown"))
+        {
+            Move(Vector2Int.down);
+        }
+        this.board.Set(this);
+    }
+
+    private void Rotate(int direction)
+    {
+        int originalRotation = this.rotationIndex;
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        RotationMethod(direction);
+    }
+
+    private void RotationMethod(int direction)
+    {
+        float cos = Mathf.Cos(Mathf.PI / 2f);
+        float sin = Mathf.Sin(Mathf.PI / 2f);
+        float[] matrix = new float[] { cos, sin, -sin, cos };
+
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.shapeData.shape)
+            {
+                case Shape.I:
+                case Shape.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * matrix[0] * direction) + (cell.y * matrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * matrix[2] * direction) + (cell.y * matrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.RoundToInt((cell.x * matrix[0] * direction) + (cell.y * matrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * matrix[2] * direction) + (cell.y * matrix[3] * direction));
+                    break;
+            }
+
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
+    }
+
+    private bool Move(Vector2Int direction)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += direction.x;
+        newPosition.y += direction.y;
+
+        bool isValid = this.board.IsValidPosition(this, newPosition);
+
+        if (isValid)
+        {
+            this.position = newPosition;
+        }
+
+        return isValid;
     }
 }
